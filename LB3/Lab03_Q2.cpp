c86bfe0fe7dd9def9d94aee99b693071365aa055
STRONGAIR030 <b89586639@gmail.com>
2025-03-11 23:05:46 +0800

    update

--- LB3/Lab03_Q2.cpp
+++ LB3/Lab03_Q2.cpp
@@ -17,6 +17,8 @@ class Term {
         this->coef = term.coef;
         this->exp = term.exp;
     };
+
+    // 交換兩個項
     void swapTerm(Term *term) {
         Term temp = *this;
         *this = *term;
@@ -37,15 +39,15 @@ class Polynomial {
     ~Polynomial() {
         delete[] termArray;
     };  // destructor
-    void displayInfo();                      // 印出多項式資料
-    void printPoly();                        // 印出多項式
-    void input();                            // 輸入多項式
-    void sortPoly();                         // 排序多項式
-    void add(Polynomial poly);               // 多項式相加
-    void addterm(Term term);                 // 新增項
-    void mult(Polynomial poly);              // 多項式相乘
-    void eval(double x);                     // 代入數字
-    void operator=(const Polynomial &poly);  // 多項式的等號重載
+    void displayInfo();                       // 印出多項式資料
+    void printPoly();                         // 印出多項式
+    void input();                             // 輸入多項式
+    void sortPoly();                          // 排序多項式
+    void add(const Polynomial &poly) const;   // 多項式相加
+    void addterm(Term term);                  // 新增項
+    void mult(const Polynomial &poly) const;  // 多項式相乘
+    void eval(double x);                      // 代入數字
+    void operator=(const Polynomial &poly);   // 多項式的等號重載
     Polynomial operator+(const Polynomial &poly) const;
 };
 
@@ -53,8 +55,9 @@ class Polynomial {
 void Polynomial::sortPoly() {
     for (int i = 0; i < itemNum; i++) {
         for (int j = 0; j < itemNum - i - 1; j++) {
+            // 次方大的往前排
             if (termArray[j].exp < termArray[j + 1].exp) {
-                termArray[j].swapTerm(&termArray[j + 1]);
+                termArray[j].swapTerm(&termArray[j + 1]);  // 交換
             }
         }
     }
@@ -62,29 +65,35 @@ void Polynomial::sortPoly() {
 
 // 新增項
 void Polynomial::addterm(Term term) {
-    Term *temp = new Term[itemNum + 1];
+    Term *temp = new Term[itemNum + 1];  // 建立比原本多一個的多項式
+
+    // 複製原本的多項式
     for (int i = 0; i < itemNum; i++) {
         temp[i] = termArray[i];
     }
-    temp[itemNum] = term;
-    delete[] termArray;
-    termArray = temp;
-    itemNum++;
+    temp[itemNum] = term;  // 新增項
+    delete[] termArray;    // 釋放記憶體
+    termArray = temp;      // 指向新的多項式
+    itemNum++;             // 項數+1
 };
 
 // 多項式的等號重載
 void Polynomial::operator=(const Polynomial &poly) {
     if (this == &poly) return;
-    this->degree = poly.degree;               // 複製次方
-    if (this->termArray) delete[] termArray;  // 刪除原本的多項式
-    this->itemNum = poly.itemNum;             // 複製係數和次方
+    this->degree = poly.degree;  // 複製次方
+    if (this->termArray) {
+        delete[] termArray;  // 釋放記憶體
+        termArray = nullptr;
+    }
+    this->itemNum = poly.itemNum;  // 複製係數和次方
     if (poly.termArray) {
-        this->termArray = new Term[itemNum];
+        this->termArray = new Term[itemNum];  // 建立新的多項式
+        // 複製多項式
         for (int i = 0; i < itemNum; i++) {
             this->termArray[i] = poly.termArray[i];
         }
     } else {
-        this->termArray = nullptr;
+        this->termArray = nullptr;  // 沒有多項式就設為nullptr
     }
 };
 
@@ -94,32 +103,44 @@ Polynomial Polynomial::operator+(const Polynomial &poly) const {
     Polynomial result;             // 相加結果
     while (1) {
         if (p1Index >= itemNum && p2Index >= poly.itemNum) break;  // 兩個多項式都加完就停止
+
+        // 因為我們的多項式都已經依照次方由大到小排序了
+        // 所以目前索引的次方項是該多項式還沒處理的最大次方項
+        // 如果兩個最大次方項的次方不相等，就直接輸出次方較大的項，因為沒有相同次方的項可以相加了
+        // 反之如果兩個多項式的次方相等就相加
+        // 重複這個步驟直到兩個多項式都加完
+
         if (p1Index >= itemNum) {
-            result.addterm(poly.termArray[p2Index]);
-            p2Index++;
+            // 如果多項式1的索引大於等於項數就直接輸出多項式2的項
+            result.addterm(poly.termArray[p2Index]);  // 新增項
+            p2Index++;                                // 多項式2的索引+1
             continue;
         } else if (p2Index >= poly.itemNum) {
-            result.addterm(termArray[p1Index]);
-            p1Index++;
+            // 如果多項式2的索引大於等於項數就直接輸出多項式1的項
+            result.addterm(termArray[p1Index]);  // 新增項
+            p1Index++;                           // 多項式1的索引+1
             continue;
         }
 
         if (termArray[p1Index].exp > poly.termArray[p2Index].exp) {
-            result.addterm(termArray[p1Index]);
-            p1Index++;
+            // 多項式1的次方大於多項式2的次方，輸出多項式1目前索引的項
+            result.addterm(termArray[p1Index]);  // 新增項
+            p1Index++;                           // 多項式1的索引+1
         } else if (termArray[p1Index].exp < poly.termArray[p2Index].exp) {
-            result.addterm(poly.termArray[p2Index]);
-            p2Index++;
+            // 多項式2的次方大於多項式1的次方，輸出多項式2目前索引的項
+            result.addterm(poly.termArray[p2Index]);  // 新增項
+            p2Index++;                                // 多項式2的索引+1
         } else {
-            Term temp;
-            temp.coef = termArray[p1Index].coef + poly.termArray[p2Index].coef;
+            // 兩個多項式的次方相等，相加並輸出
+            Term temp;                                                           // 暫存結果
+            temp.coef = termArray[p1Index].coef + poly.termArray[p2Index].coef;  // 係數相加
             temp.exp = termArray[p1Index].exp;
-            result.addterm(temp);
-            p1Index++;
-            p2Index++;
+            result.addterm(temp);  // 新增項
+            p1Index++;             // 多項式1的索引+1
+            p2Index++;             // 多項式2的索引+1
         }
     }
-    return result;
+    return result;  // 回傳相加結果
 }
 
 // 輸入多項式
@@ -129,9 +150,9 @@ void Polynomial::input() {
     // 先輸入係數再輸入次方(用空格隔開)
     // 如 3x^2 + 2x + 1 輸入 3 2 2 1 1 0
     cout << "輸入 " << name << " 的項數" << " : ";
-    cin >> itemNum;
+    cin >> itemNum;  // 輸入項數
     cout << "輸入 " << name << " : ";
-    termArray = new Term[itemNum];
+    termArray = new Term[itemNum];  // 建立多項式
     for (int i = 0; i < itemNum; i++) {
         cin >> termArray[i].coef >> termArray[i].exp;              // 輸入係數和次方
         if (termArray[i].exp > degree) degree = termArray[i].exp;  // 找出最大次方
@@ -161,7 +182,7 @@ void Polynomial::displayInfo() {
 };
 
 // 多項式相加
-void Polynomial::add(Polynomial poly) {
+void Polynomial::add(const Polynomial &poly) const {
     Polynomial result;                            // 相加結果
     result = *this + poly;                        // 相加
     cout << name << " + " << poly.name << " = ";  // 印出多項式名稱
@@ -170,7 +191,7 @@ void Polynomial::add(Polynomial poly) {
 }
 
 // 多項式相乘
-void Polynomial::mult(Polynomial poly) {
+void Polynomial::mult(const Polynomial &poly) const {
     Polynomial result;  // 相乘結果
     for (int i = 0; i < itemNum; i++) {
         Polynomial temp;  // 暫存結果
